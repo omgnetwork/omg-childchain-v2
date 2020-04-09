e69ca9813b0bb0bd9f5a2c6593e63bd74bd4f42a
Ino Murko <ino.murko@outlook.com>
2020-04-09 21:11:24 +0200

    test alarm handler

---
+++ apps/engine/test/engine/ethereum/monitor/alarm_handler_test.exs
@@ -0,0 +1,28 @@
+defmodule Engine.Ethereum.Monitor.AlarmHandlerTest do
+  @moduledoc false
+  use ExUnit.Case, async: true
+  import ExUnit.CaptureLog, only: [capture_log: 1]
+  alias Engine.Ethereum.Monitor.AlarmHandler
+
+  test "that when a clear ethereum connection error alarm arrives we send a message to the consumer" do
+    clear_alarm_event = {:clear_alarm, {:ethereum_connection_error, %{}}}
+    alarm_handler = %AlarmHandler{consumer: self()}
+
+    capture_log(fn ->
+      AlarmHandler.handle_event(clear_alarm_event, alarm_handler)
+      assert_receive {:"$gen_cast", :start_child}
+    end)
+  end
+
+  test "that when some other event arrives we ignore it (we don't receive any messages)" do
+    event = {:clear_alarm, {:yolo, %{}}}
+    alarm_handler = %AlarmHandler{consumer: self()}
+    AlarmHandler.handle_event(event, alarm_handler)
+
+    receive do
+      _ -> assert false
+    after
+      100 -> assert true
+    end
+  end
+end
