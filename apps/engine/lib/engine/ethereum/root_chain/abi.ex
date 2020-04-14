5c0812f19484561e721ac2c159e307fd485cda97
Ino Murko <ino.murko@outlook.com>
2020-04-14 11:41:10 +0200

    Inomurko/abi events (#20)
    * abi decoding/encoding
    
    * abi decoding/encoding
    
    * dialyzer
    
    * Update encoding.ex

---
+++ apps/engine/lib/engine/ethereum/root_chain/abi.ex
@@ -0,0 +1,83 @@
+defmodule Engine.Ethereum.RootChain.Abi do
+  @moduledoc """
+  Functions that provide ethereum log decoding 
+  """
+  alias Engine.Encoding
+  alias Engine.Ethereum.RootChain.AbiEventSelector
+  alias Engine.Ethereum.RootChain.AbiFunctionSelector
+  alias Engine.Ethereum.RootChain.Fields
+
+  def decode_function(enriched_data, signature) do
+    "0x" <> data = enriched_data
+    <<method_id::binary-size(4), _::binary>> = :keccakf1600.hash(:sha3_256, signature)
+    method_id |> Encoding.to_hex() |> Kernel.<>(data) |> Encoding.from_hex() |> decode_function()
+  end
+
+  def decode_function(enriched_data) do
+    function_specs =
+      Enum.reduce(AbiFunctionSelector.module_info(:exports), [], fn
+        {:module_info, 0}, acc -> acc
+        {function, 0}, acc -> [apply(AbiFunctionSelector, function, []) | acc]
+        _, acc -> acc
+      end)
+
+    {function_spec, data} = ABI.find_and_decode(function_specs, enriched_data)
+    decode_function_call_result(function_spec, data)
+  end
+
+  def decode_log(log) do
+    event_specs =
+      Enum.reduce(AbiEventSelector.module_info(:exports), [], fn
+        {:module_info, 0}, acc -> acc
+        {function, 0}, acc -> [apply(AbiEventSelector, function, []) | acc]
+        _, acc -> acc
+      end)
+
+    topics =
+      Enum.map(log["topics"], fn
+        nil -> nil
+        topic -> Encoding.from_hex(topic)
+      end)
+
+    {event_spec, data} =
+      ABI.Event.find_and_decode(
+        event_specs,
+        Enum.at(topics, 0),
+        Enum.at(topics, 1),
+        Enum.at(topics, 2),
+        Enum.at(topics, 3),
+        Encoding.from_hex(log["data"])
+      )
+
+    data
+    |> Enum.into(%{}, fn {key, _type, _indexed, value} -> {key, value} end)
+    |> Fields.rename(event_spec)
+    |> common_parse_event(log)
+  end
+
+  def common_parse_event(
+        result,
+        %{"blockNumber" => eth_height, "transactionHash" => root_chain_txhash, "logIndex" => log_index} = event
+      ) do
+    # NOTE: we're using `put_new` here, because `merge` would allow us to overwrite data fields in case of conflict
+    result
+    |> Map.put_new(:eth_height, Encoding.int_from_hex(eth_height))
+    |> Map.put_new(:root_chain_txhash, Encoding.from_hex(root_chain_txhash))
+    |> Map.put_new(:log_index, Encoding.int_from_hex(log_index))
+    # just copy `event_signature` over, if it's present (could use tidying up)
+    |> Map.put_new(:event_signature, event[:event_signature])
+  end
+
+  defp decode_function_call_result(function_spec, [values]) when is_tuple(values) do
+    function_spec.input_names
+    |> Enum.zip(Tuple.to_list(values))
+    |> Enum.into(%{})
+    |> Fields.rename(function_spec)
+  end
+
+  defp decode_function_call_result(function_spec, values) do
+    function_spec.input_names
+    |> Enum.zip(values)
+    |> Enum.into(%{})
+  end
+end
---
+++ apps/engine/lib/engine/ethereum/root_chain/abi_function_selectors.ex
@@ -0,0 +1,150 @@
+# Copyright 2019-2020 OmiseGO Pte Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+defmodule Engine.Ethereum.RootChain.AbiFunctionSelector do
+  @moduledoc """
+
+  We define Solidity Function selectors that help us decode returned values from function calls
+  """
+  def start_standard_exit() do
+    %ABI.FunctionSelector{
+      function: "startStandardExit",
+      input_names: ["utxoPos", "rlpOutputTx", "outputTxInclusionProof"],
+      inputs_indexed: nil,
+      method_id: <<112, 224, 20, 98>>,
+      returns: [],
+      type: :function,
+      types: [tuple: [{:uint, 256}, :bytes, :bytes]]
+    }
+  end
+
+  def start_in_flight_exit() do
+    %ABI.FunctionSelector{
+      function: "startInFlightExit",
+      input_names: ["inFlightTx", "inputTxs", "inputUtxosPos", "inputTxsInclusionProofs", "inFlightTxWitnesses"],
+      inputs_indexed: nil,
+      method_id: <<90, 82, 133, 20>>,
+      returns: [],
+      type: :function,
+      types: [
+        tuple: [
+          :bytes,
+          {:array, :bytes},
+          {:array, {:uint, 256}},
+          {:array, :bytes},
+          {:array, :bytes}
+        ]
+      ]
+    }
+  end
+
+  # min_exit_period/0, get_version/0, exit_games/0, vaults/0 are
+  # victims of unfortinate bug: https://github.com/poanetwork/ex_abi/issues/25
+  # All these selectors were intially pulled in with
+  # `ABI.parse_specification(contract_abi_json_decoded,include_events?: true)`
+  # and later modified so that `types` hold what `returns` should have because of
+  # issue 25.
+  # the commented properties of the struct is what it was generated,
+  # the new types were added to mitigate the bug.
+  def min_exit_period() do
+    %ABI.FunctionSelector{
+      function: "minExitPeriod",
+      input_names: ["min_exit_period"],
+      inputs_indexed: nil,
+      method_id: <<212, 162, 180, 239>>,
+      # returns: [uint: 256],
+      type: :function,
+      # types: []
+      types: [uint: 256]
+    }
+  end
+
+  def get_version() do
+    %ABI.FunctionSelector{
+      function: "getVersion",
+      input_names: ["version"],
+      inputs_indexed: nil,
+      method_id: <<13, 142, 110, 44>>,
+      # returns: [:string],
+      type: :function,
+      # types: []
+      types: [:string]
+    }
+  end
+
+  def exit_games() do
+    %ABI.FunctionSelector{
+      function: "exitGames",
+      input_names: ["exit_game_address"],
+      inputs_indexed: nil,
+      method_id: <<175, 7, 151, 100>>,
+      # returns: [:address],
+      type: :function,
+      # types: [uint: 256]
+      types: [:address]
+    }
+  end
+
+  def vaults() do
+    %ABI.FunctionSelector{
+      function: "vaults",
+      input_names: ["vault_address"],
+      inputs_indexed: nil,
+      method_id: <<140, 100, 234, 74>>,
+      # returns: [:address],
+      type: :function,
+      # types: [uint: 256]
+      types: [:address]
+    }
+  end
+
+  def child_block_interval() do
+    %ABI.FunctionSelector{
+      function: "childBlockInterval",
+      input_names: ["child_block_interval"],
+      inputs_indexed: nil,
+      method_id: <<56, 169, 224, 188>>,
+      # returns: [uint: 256],
+      type: :function,
+      # types: []
+      types: [uint: 256]
+    }
+  end
+
+  def next_child_block() do
+    %ABI.FunctionSelector{
+      function: "nextChildBlock",
+      input_names: ["block_number"],
+      inputs_indexed: nil,
+      method_id: <<76, 168, 113, 79>>,
+      # returns: [uint: 256],
+      type: :function,
+      # types: []
+      types: [uint: 256]
+    }
+  end
+
+  def blocks() do
+    %ABI.FunctionSelector{
+      function: "blocks",
+      input_names: ["block_hash", "block_timestamp"],
+      inputs_indexed: nil,
+      method_id: <<242, 91, 63, 153>>,
+      # returns: [bytes: 32, uint: 256],
+      type: :function,
+      # types: [uint: 256]
+      types: [bytes: 32, uint: 256]
+    }
+  end
+end
