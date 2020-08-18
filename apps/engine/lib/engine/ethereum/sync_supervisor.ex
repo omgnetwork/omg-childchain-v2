003f4e6071cfff087290e29bd2ae849cce291dbe
achiurizo <hallo+github@achiurizo.co>
2020-08-18 14:21:20 -0700

    connect piggyback

--- apps/engine/lib/engine/ethereum/sync_supervisor.ex
+++ apps/engine/lib/engine/ethereum/sync_supervisor.ex
@@ -6,6 +6,7 @@ defmodule Engine.Ethereum.SyncSupervisor do
 
   alias Engine.Callbacks.Deposit
   alias Engine.Callbacks.ExitStarted
+  alias Engine.Callbacks.Piggyback
   alias Engine.Configuration
   alias Engine.Ethereum.ChildObserver
   alias Engine.Ethereum.Event.Aggregator
@@ -74,15 +75,15 @@ defmodule Engine.Ethereum.SyncSupervisor do
       #   get_events_callback: &Aggregator.in_flight_exit_started/2,
       #   process_events_callback: &exit_and_ignore_validities/1
       # ),
-      # EthereumEventListener.prepare_child(
-      #   ets: ListenerStorage.listener_checkin(),
-      #   metrics_collection_interval: metrics_collection_interval,
-      #   ethereum_events_check_interval_ms: ethereum_events_check_interval_ms,
-      #   contract_deployment_height: contract_deployment_height,
-      #   service_name: :piggyback,
-      #   get_events_callback: &Aggregator.in_flight_exit_piggybacked/2,
-      #   process_events_callback: &exit_and_ignore_validities/1
-      # ),
+       Listener.prepare_child(
+         ets: ListenerStorage.listener_checkin(),
+         metrics_collection_interval: metrics_collection_interval,
+         coordinator_eth_height_check_interval_ms: coordinator_eth_height_check_interval_ms,
+         contract_deployment_height: contract_deployment_height,
+         service_name: :piggyback,
+         get_events_callback: &Aggregator.in_flight_exit_piggybacked/2,
+         process_events_callback: &Piggyback.callback/2
+       ),
        Listener.prepare_child(
          ets: ListenerStorage.listener_checkin(),
          metrics_collection_interval: metrics_collection_interval,
