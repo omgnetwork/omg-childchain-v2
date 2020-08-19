9f4c445ef4fc75dba07816dcd4a2bb438254a77d
Ayrat Badykov <ayratin555@gmail.com>
2020-08-19 17:22:51 +0300

    add a happy path test for fee controller

---
+++ apps/api/test/api/v1/controllers/fee_test.exs
@@ -0,0 +1,84 @@
+defmodule API.V1.Controller.FeeTest do
+  use Engine.DB.DataCase, async: true
+
+  alias API.V1.Controller.Fee
+  alias Engine.DB.Fee, as: DbFees
+
+  setup_all do
+    fee_specs = %{
+      1 => %{
+        Base.decode16!("0000000000000000000000000000000000000000") => %{
+          amount: 1,
+          subunit_to_unit: 1_000_000_000_000_000_000,
+          pegged_amount: 1,
+          pegged_currency: "USD",
+          pegged_subunit_to_unit: 100,
+          updated_at: DateTime.from_unix!(1_546_336_800)
+        },
+        Base.decode16!("0000000000000000000000000000000000000001") => %{
+          amount: 2,
+          subunit_to_unit: 1_000_000_000_000_000_000,
+          pegged_amount: 1,
+          pegged_currency: "USD",
+          pegged_subunit_to_unit: 100,
+          updated_at: DateTime.from_unix!(1_546_336_800)
+        }
+      },
+      2 => %{
+        Base.decode16!("0000000000000000000000000000000000000000") => %{
+          amount: 2,
+          subunit_to_unit: 1_000_000_000_000_000_000,
+          pegged_amount: 1,
+          pegged_currency: "USD",
+          pegged_subunit_to_unit: 100,
+          updated_at: DateTime.from_unix!(1_546_336_800)
+        }
+      }
+    }
+
+    params = %{term: fee_specs, type: "current_fees"}
+
+    {:ok, _fees} = DbFees.insert(params)
+
+    :ok
+  end
+
+  describe "all/1" do
+    test "it returns fees" do
+      assert {:ok,
+              %{
+                "1" => [
+                  %{
+                    amount: 1,
+                    currency: "0x0000000000000000000000000000000000000000",
+                    pegged_amount: 1,
+                    pegged_currency: "USD",
+                    pegged_subunit_to_unit: 100,
+                    subunit_to_unit: 1_000_000_000_000_000_000,
+                    updated_at: ~U[2019-01-01 10:00:00Z]
+                  },
+                  %{
+                    amount: 2,
+                    currency: "0x0000000000000000000000000000000000000001",
+                    pegged_amount: 1,
+                    pegged_currency: "USD",
+                    pegged_subunit_to_unit: 100,
+                    subunit_to_unit: 1_000_000_000_000_000_000,
+                    updated_at: ~U[2019-01-01 10:00:00Z]
+                  }
+                ],
+                "2" => [
+                  %{
+                    amount: 2,
+                    currency: "0x0000000000000000000000000000000000000000",
+                    pegged_amount: 1,
+                    pegged_currency: "USD",
+                    pegged_subunit_to_unit: 100,
+                    subunit_to_unit: 1_000_000_000_000_000_000,
+                    updated_at: ~U[2019-01-01 10:00:00Z]
+                  }
+                ]
+              }} == Fee.all(%{})
+    end
+  end
+end
