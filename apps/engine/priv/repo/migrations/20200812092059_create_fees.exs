7acc08d8b0e21ea76cc10d387e00afc636a9cec3
Ayrat Badykov <ayratin555@gmail.com>
2020-08-19 11:09:55 +0300

    do not create a new migration

--- apps/engine/priv/repo/migrations/20200812092059_create_fees.exs
+++ apps/engine/priv/repo/migrations/20200812092059_create_fees.exs
@@ -5,8 +5,12 @@ defmodule Engine.Repo.Migrations.CreateFees do
     create table(:fees, primary_key: false) do
       add(:hash, :string, primary_key: true)
       add(:term, :binary, null: false)
+      add(:type, :string, null: false)
 
       timestamps(type: :utc_datetime)
     end
+
+    create(index(:fees, [:type]))
+    create(index(:fees, [:inserted_at]))
   end
 end
