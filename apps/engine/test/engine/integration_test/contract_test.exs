2e6d0148ef98a9832b03627762b3c7e8f927863e
Ayrat Badykov <ayratin555@gmail.com>
2020-10-01 10:06:14 +0300

    fix contract_test

--- apps/engine/test/engine/integration_test/contract_test.exs
+++ apps/engine/test/engine/integration_test/contract_test.exs
@@ -25,15 +25,15 @@ defmodule ContractTest do
         ethereumex: [url: "not used because env var"],
         engine: [
           rpc_url: "http://localhost:#{port}",
-          authority_address: "0x1ab323bcd956992806594faa72e5ed41509a9662",
+          authority_address: "0xf91d00cc5906c355b6c8a04d9d940c4adc64cb1c",
           plasma_framework: "0x97ba80836092c734d400acb79e310bcd4776dddb",
-          eth_vault: "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
-          erc20_vault: "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
-          payment_exit_game: "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",
+          eth_vault: "0xf39aba0a60dd1be8f9ddf2cc2104e8c3a8ba5670",
+          erc20_vault: "0xe520b5e3df580f9015141152e152ea5edf119a74",
+          payment_exit_game: "0xdd2860dd8f182f90870383a98ddaf63fdb00573e",
           min_exit_period_seconds: 20,
-          contract_semver: "2.0.0+8468675",
+          contract_semver: "2.0.0+ddbd40b",
           child_block_interval: 1000,
-          contract_deployment_height: 131
+          contract_deployment_height: 120
         ]
       ]
 
