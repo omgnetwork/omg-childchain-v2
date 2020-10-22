eda2ea20f3c7a442cdc032ec4058fc84fdebc354
Ino Murko <ino@omg.network>
2020-10-22 16:38:59 +0200

    move fee server to a isolated supervisor, cleanup vars

---
+++ apps/engine/lib/engine/supervisor.ex
@@ -0,0 +1,27 @@
+defmodule Engine.Supervisor do
+  @moduledoc """
+   Engine top level supervisor is supervising FeeServer.
+  """
+  use Supervisor
+
+  alias Engine.Configuration
+  alias Engine.Fee.Server, as: FeeServer
+  require Logger
+
+  def start_link(args) do
+    Supervisor.start_link(__MODULE__, args, name: __MODULE__)
+  end
+
+  def init(_args) do
+    fee_server_opts = Configuration.fee_server_opts()
+
+    children = [
+      {FeeServer, fee_server_opts}
+    ]
+
+    opts = [strategy: :one_for_one]
+
+    _ = Logger.info("Starting #{inspect(__MODULE__)}")
+    Supervisor.init(children, opts)
+  end
+end
