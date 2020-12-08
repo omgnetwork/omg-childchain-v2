c78bc2b08855a6141882a400d098e1d5c33fc76c
Michael Captain <michael@omg.network>
2020-12-08 08:31:22 +0000

    feat: send telemetry events to Datadog (#181)
    Port of https://github.com/omgnetwork/elixir-omg/pull/1766 to Childchain
    v2.

--- apps/api/lib/api/v1/error_handler.ex
+++ apps/api/lib/api/v1/error_handler.ex
@@ -1,6 +1,6 @@
 defmodule API.V1.ErrorHandler do
   @moduledoc """
-  Handle errors raised by plugs and format them before responding
+  Handle errors raised by plugs and format them before sending a telemetry event and responding
   """
 
   alias API.Plugs.Responder
@@ -24,6 +24,14 @@ defmodule API.V1.ErrorHandler do
   end
 
   defp respond(conn, error) do
+    {:error, error_code} = error
+
+    :telemetry.execute(
+      [:web, :fallback],
+      %{error: 1},
+      %{error_code: error_code, route: Map.fetch(conn.private, :plug_route)}
+    )
+
     conn
     |> Conn.put_status(400)
     |> Conn.assign(:response, error)
